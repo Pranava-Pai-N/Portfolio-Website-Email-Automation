faa837327783ee0bc863ec036ee7bbf6e810bf54
Pranav Pai N <pranavpai0309@gmail.com>
2025-06-17 22:03:58 +0530

    Feat : Added specific origins to protect the feature

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ["*"],
+    allow_origins= ["https://simple-portfolio-website-git-main-pranav-pais-projects.vercel.app","https://simple-portfolio-website-mwcbtri8d-pranav-pais-projects.vercel.app","https://simple-portfolio-website-iota.vercel.app"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
